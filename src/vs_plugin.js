ea224011ac89a5507bb1b40748bee596c43deb5f
Lukas <lukas.gromer@uni-ulm.de>
2025-02-20 18:38:00 +0100

    Moved format definition into its own file

--- src/vs_plugin.js
+++ src/vs_plugin.js
@@ -3,6 +3,7 @@ const { debug } = require('console');
 const path = require('node:path');
 const ex = require("./export.js");
 const im = require("./import.js");
+const format_definition = require("./format_definition.js");
 
 
 
@@ -23,10 +24,6 @@ Plugin.register('vs_plugin', {
 
     onload() {
         //Init additional Attribute Properties
-
-
-
-
         let game_path_setting = new Setting("game_path", {
             name: "Game Path",
             description: "The path to your Vintage Story game folder. This is the folder that contains the assets, mods and lib folders.",
@@ -54,9 +51,6 @@ Plugin.register('vs_plugin', {
             }
         })
 
-        
-
-
         let codecVS = new Codec("codecVS", {
             name: "Vintage Story Codec",
             extension: "json",
@@ -76,53 +70,7 @@ Plugin.register('vs_plugin', {
             }
         })
 
-        let formatVS = new ModelFormat("formatVS", {
-            name: "Vintage Story Base Format",
-            codec: codecVS,
-            icon: "fa-cookie-bite",
-            box_uv: false,
-            optional_box_uv: false,
-            single_texture: false,
-            single_texture_default: false,
-            per_group_texture: false,
-            per_texture_uv_size: true,
-            model_identifier: false,
-            legacy_editable_file_name: false,
-            parent_model_id: false, //Use this for backdrops? false for now
-            vertex_color_ambient_occlusion: false,
-            animated_textures: false, // NOt sure if supported by VS
-            bone_rig: true,
-            centered_grid: true,
-            rotate_cubes: true,
-            stretch_cubes: false,
-            integer_size: false,
-            meshes: false,
-            texture_meshes: false,
-            locators: false, // Not quite sure what that is
-            rotation_limit: false,
-            rotation_snap: false,
-            uv_rotation: true,
-            java_face_properties: false,
-            select_texture_for_particles: false,
-            texture_mcmeta: false,
-            bone_binding_expression: false, // Revisit for animation
-            animation_files: false,
-            texture_folder: true,
-            image_editor: false, // Setting this to true removes the object outliner?!?!
-            edit_mode: true,
-            paint_mode: true,
-            display_mode: false, // Only some Minecraft Skin stuff it seems
-            animation_mode: true,
-            pose_mode: false,
-            animation_controllers: true,
-            box_uv_float_size: false,
-            java_cube_shading_properties: false,
-            cullfaces: false, // Not sure if Vintage Story supports this
-            // node_name_regex: null,
-            render_sides: "front", //Seems right to me but I havent tested how VS does it
-
-
-        })
+        let formatVS = format_definition(codecVS)
 
 
         exportAction = new Action('exportVS', {
