76e0e8ee31db7268b9a1a12ec295cca99039c83c
Darkluke1111 <lukas.gromer@freenet.de>
2025-02-22 13:29:39 +0100

    Added zipping plugin to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
- const path = require('path');
+const path = require('path');
+var ZipPlugin = require('zip-webpack-plugin');
 
- module.exports = {
-   target: 'node', 
-   entry: './src/vs_plugin.js', 
-   output: {
-     filename: 'vs_plugin.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   optimization: {
+module.exports = {
+    mode: 'development',
+    target: 'node',
+    entry: './src/vs_plugin.js',
+    output: {
+        filename: 'vs_plugin.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new ZipPlugin({
+            filename: 'UnpackMe_vs_plugin.zip'
+        })
+    ],
+    optimization: {
         minimize: false
-    }
-   // Additional configuration goes here
- };
+    },
+    // Additional configuration goes here
+};
